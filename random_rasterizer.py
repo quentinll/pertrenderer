767efdb1a435c18fff19c704427c1eda4a69304f
quentin <quentin.lelidec@orange.fr>
2021-03-01 14:48:30 +0100

    generic implementation of random approximations

--- random_rasterizer.py
+++ random_rasterizer.py
@@ -23,100 +23,20 @@ from pytorch3d.renderer import (
     HardPhongShader,
     SoftSilhouetteShader,
     SoftPhongShader,
+    hard_rgb_blend,
+    softmax_rgb_blend,
     TexturesVertex,
     BlendParams
 )
 
+from torch.distributions.transforms import SigmoidTransform
 
-class randomHeaviside(Function):
-    
-    @staticmethod
-    def forward(ctx,distances,nb_samples = 1,noise_intensity = 1e-1, noise_type ="gaussian"):
-      device = distances.device
-      dist_size = distances.size()
-      noise_dict ={"gaussian": torch.tensor(0)}
-      noise_type = noise_dict[noise_type]
-      if noise_type == noise_dict["gaussian"]:
-        if nb_samples >0:
-            noise = torch.normal(mean = torch.zeros((nb_samples,dist_size[0],dist_size[1],dist_size[2],dist_size[3]),device=device),std = 1. )
-        else:
-            return "error, unknown analytical expression of expectation of gaussian pertubation heaviside"
-      elif noise_type == noise_dict["logistic"]:
-        if nb_samples >0:
-            base_distribution = torch.distributions.uniform.Uniform(0, 1)
-            transforms = [SigmoidTransform().inv]
-            logistic = torch.distributions.transformed_distribution.TransformedDistribution(base_distribution, transforms)
-            noise  = logistic.sample((nb_samples,dist_size[0],dist_size[1],dist_size[2],dist_size[3]))
-      else:
-        print("noise type not implemented")
-      maps = distances + noise_intensity*noise
-      maps = torch.heaviside(maps, values = torch.ones(maps.size(), device=device))
-      ctx.save_for_backward(maps,noise,torch.tensor(noise_intensity),noise_type)
-      map = maps.mean(dim=0)
-      return map
-    
-    @staticmethod
-    def backward(ctx, grad_l):
-      '''
-      Compute derivatives of the solution of the QCQP with respect to 
-      '''
-      grad_dist = None
-      maps, noise, noise_intensity, noise_type = ctx.saved_tensors
-      noise_dict ={"gaussian": torch.tensor(0)}
-      if noise_type == noise_dict["gaussian"]:
-        grad_maps = maps * noise/noise_intensity
-      else:
-        print("noise_type not implemented")
-      grad_maps = grad_maps.mean(dim=0)
-      if ctx.needs_input_grad[0]:
-          grad_dist = grad_maps*grad_l
-      return grad_dist, None, None, None
-
-class randomArgmax(Function):
-    
-    @staticmethod
-    def forward(ctx,z,nb_samples = 1,noise_intensity = 1e-1, noise_type ="gaussian"):
-      device = z.device
-      z_size = z.size()
-      noise_dict ={"gaussian": torch.tensor(0),"gumbel": torch.tensor(1)}
-      noise_type = noise_dict[noise_type]
-      if noise_type == noise_dict["gaussian"]:
-        noise = torch.normal(mean = torch.zeros((nb_samples,z_size[0],z_size[1],z_size[2],z_size[3]),device=device),std = 1. )
-      elif noise_type == noise_dict["gumbel"]:
-        if nb_samples > 0:
-            m = torch.distributions.gumbel.Gumbel(torch.tensor([0.]), torch.tensor([1.]))
-            noise = m.sample((nb_samples,dist_size[0],dist_size[1],dist_size[2],dist_size[3]))
-      else:
-        print("noise type not implemented")
-      z_pert = z + noise_intensity*noise
-      _, indices = torch.max(z_pert, dim =-1, keepdim=True)
-      weights = torch.zeros(z_pert.size(), device = device)
-      weights.scatter_(-1, indices, 1)
-      ctx.save_for_backward(weights,noise,torch.tensor(noise_intensity),noise_type)
-      weight = weights.mean(dim = 0)
-      return weight
-    
-    @staticmethod
-    def backward(ctx, grad_l):
-      '''
-      Compute derivatives of the solution of the QCQP with respect to 
-      '''
-      grad_z = None
-      weights, noise, noise_intensity, noise_type = ctx.saved_tensors
-      noise_dict ={"gaussian": torch.tensor(0)}
-      if noise_type == noise_dict["gaussian"]:
-        grad_z = torch.matmul(weights.unsqueeze(-1),noise.unsqueeze(-2))/noise_intensity
-      else:
-        print("noise_type not implemented")
-      grad_z = grad_z.mean(dim=0)
-      if ctx.needs_input_grad[0]:
-        grad_z = torch.matmul(grad_l,grad_z)
-      return grad_z, None, None, None
+from smoothrast import SoftRast
+from smoothagg import SoftAgg
 
 
-def random_rgb_blend(
-    colors, fragments, blend_params, znear: float = 1.0, zfar: float = 100, nb_samples: int = 1
-) -> torch.Tensor:
+def smooth_rgb_blend(
+    colors, fragments, smoothrast, smoothagg, blend_params, znear: float = 1.0, zfar: float = 100) -> torch.Tensor:
 
     N, H, W, K = fragments.pix_to_face.shape
     device = fragments.pix_to_face.device
@@ -134,24 +54,28 @@ def random_rgb_blend(
     mask = fragments.pix_to_face >= 0
 
     # Perturbed Heaviside function map based on the distance of the pixel to the face.
-    randomheavi = randomHeaviside().apply
-    prob_map = randomheavi(-fragments.dists, nb_samples, blend_params.sigma,"gaussian")*mask
-    alpha = torch.prod((1.0 - prob_map), dim=-1)
-    z_inv = (zfar - fragments.zbuf) / (zfar - znear) * mask
-    z_inv_max = torch.max(z_inv, dim=-1).values[..., None].clamp(min=eps)
+    prob_map = smoothrast.rasterize(fragments.dists)*mask
+    #randomheavi = randomHeaviside().apply
+    #prob_map = randomheavi(-fragments.dists, nb_samples, blend_params.sigma,noise_type)*mask
+    alpha_chan = torch.prod((1.0 - prob_map), dim=-1)
+    #z_inv = (zfar - fragments.zbuf) / (zfar - znear) * mask
+    #z_inv_max = torch.max(z_inv, dim=-1).values[..., None].clamp(min=eps)
     #need to balance between color map and depth ?Why log ?
-    z_map = 1.*torch.log(eps+prob_map)+ (z_inv-z_inv_max)/ blend_params.gamma
+    #z_map = 1.*torch.log(eps+prob_map)+ (z_inv-z_inv_max)/ blend_params.gamma
+    #z_map = ((1./alpha)*torch.log(1e-12+prob_map)+ z_inv)/ blend_params.gamma
     #add background
-    z_map =torch.cat((z_map,torch.ones((z_map.size()[0],z_map.size()[1],z_map.size()[2],1),device=device)*(eps - z_inv_max) / blend_params.gamma),dim=-1)
+    #z_map =torch.cat((z_map,torch.ones((z_map.size()[0],z_map.size()[1],z_map.size()[2],1),device=device)*(eps - z_inv_max) / blend_params.gamma),dim=-1)
+    #z_map =torch.cat((z_map,torch.ones((z_map.size()[0],z_map.size()[1],z_map.size()[2],1),device=device)*eps / blend_params.gamma),dim=-1)
 
-    randomarg = randomArgmax.apply
-    randomax = randomarg(z_map, nb_samples, blend_params.gamma, "gaussian")
+    #randomarg = randomArgmax.apply
+    #randomax = randomarg(z_map, nb_samples, blend_params.gamma, noise_type)
+    randomax = smoothagg.aggregate(fragments.zbuf,zfar,znear,prob_map,mask)
     wz,wb = randomax[...,:-1],randomax[...,-1:]
     weighted_colors = (wz[..., None] * colors).sum(dim=-2)
-
+    #print("colors",colors[0,0:2,0:2,:])
     weighted_background = wb * background
     pixel_colors[..., :3] = (weighted_colors + weighted_background)
-    pixel_colors[..., 3] = 1.0 - alpha
+    pixel_colors[..., 3] = 1.0 - alpha_chan
 
     return pixel_colors
 
@@ -169,7 +93,13 @@ class RandomPhongShader(nn.Module):
     """
 
     def __init__(
-        self, device="cpu", cameras=None, lights=None, materials=None, blend_params=None,nb_samples=1
+        self,
+        device="cpu",
+        cameras=None,
+        lights=None, materials=None,
+        smoothrast=SoftRast(),
+        smoothagg=SoftAgg(),
+        blend_params=None
     ):
         super().__init__()
         self.lights = lights if lights is not None else PointLights(device=device)
@@ -178,7 +108,8 @@ class RandomPhongShader(nn.Module):
         )
         self.cameras = cameras
         self.blend_params = blend_params if blend_params is not None else BlendParams()
-        self.nb_samples = nb_samples
+        self.smoothrast = smoothrast
+        self.smoothagg = smoothagg
 
     def to(self, device):
         # Manually move to device modules which are not subclasses of nn.Module
@@ -207,7 +138,78 @@ class RandomPhongShader(nn.Module):
         )
         znear = kwargs.get("znear", getattr(cameras, "znear", 1.0))
         zfar = kwargs.get("zfar", getattr(cameras, "zfar", 100.0))
-        images = random_rgb_blend(
-            colors, fragments, blend_params, znear=znear, zfar=zfar, nb_samples = self.nb_samples
+        images = smooth_rgb_blend(
+            colors, fragments, blend_params, znear=znear, zfar=zfar, nb_samples = self.nb_samples, alpha = self.alpha, noise_type= self.noise_type
         )
         return images
+    
+class RandomSimpleShader(nn.Module):
+
+    def __init__(
+        self,
+        device="cpu",
+        cameras=None,
+        lights=None,
+        materials=None,
+        smoothrast=SoftRast(),
+        smoothagg=SoftAgg(),
+        blend_params=None
+    ):
+        super().__init__()
+        self.lights = lights if lights is not None else PointLights(device=device)
+        self.materials = (
+            materials if materials is not None else Materials(device=device)
+        )
+        self.cameras = cameras
+        self.blend_params = blend_params if blend_params is not None else BlendParams()
+        self.smoothrast = smoothrast
+        self.smoothagg = smoothagg
+
+    def to(self, device):
+        # Manually move to device modules which are not subclasses of nn.Module
+        self.cameras = self.cameras.to(device)
+        self.materials = self.materials.to(device)
+        self.lights = self.lights.to(device)
+
+    def forward(self, fragments, meshes, **kwargs) -> torch.Tensor:
+        cameras = kwargs.get("cameras", self.cameras)
+        if cameras is None:
+            msg = "Cameras must be specified either at initialization \
+                or in the forward pass of SoftPhongShader"
+            raise ValueError(msg)
+
+        texels = meshes.sample_textures(fragments)
+        blend_params = kwargs.get("blend_params", self.blend_params)
+        znear = kwargs.get("znear", getattr(cameras, "znear", 1.0))
+        zfar = kwargs.get("zfar", getattr(cameras, "zfar", 100.0))
+        images = smooth_rgb_blend(
+            texels, fragments, self.smoothrast, self.smoothagg, blend_params, znear=znear, zfar=zfar
+        )
+        return images
+    
+    def update_smoothing(self,sigma=4e-4,gamma=4e-2,alpha =1.):
+        self.smoothrast.update_smoothing(sigma)
+        self.smoothagg.update_smoothing(gamma,alpha)
+        
+        
+class SimpleShader(nn.Module):
+    def __init__(self, device="cpu", blend_params=None):
+        super().__init__()
+        self.blend_params = blend_params if blend_params is not None else BlendParams()
+
+    def forward(self, fragments, meshes, **kwargs) -> torch.Tensor:
+        blend_params = kwargs.get("blend_params", self.blend_params)
+        texels = meshes.sample_textures(fragments)
+        images = hard_rgb_blend(texels, fragments, blend_params)
+        return images 
+    
+class SoftSimpleShader(nn.Module):
+    def __init__(self, device="cpu", blend_params=None):
+        super().__init__()
+        self.blend_params = blend_params if blend_params is not None else BlendParams()
+
+    def forward(self, fragments, meshes, **kwargs) -> torch.Tensor:
+        blend_params = kwargs.get("blend_params", self.blend_params)
+        texels = meshes.sample_textures(fragments)
+        images = softmax_rgb_blend(texels, fragments, blend_params)
+        return images 
--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ import ast
 from pathlib import Path
 import torch
 from tqdm import tqdm
+from torch.autograd import Function
 from pytorch3d.renderer import (
     look_at_view_transform,
     OpenGLPerspectiveCameras, 
@@ -27,13 +28,27 @@ from pytorch3d.renderer import (
     HardPhongShader,
     SoftSilhouetteShader,
     SoftPhongShader,
+    Textures,
     TexturesVertex,
     BlendParams
 )
-from pytorch3d.transforms import Rotate, euler_angles_to_matrix, random_rotations,so3_exponential_map,so3_log_map,so3_rotation_angle,so3_relative_angle
-from random_rasterizer import RandomPhongShader
+from pytorch3d.transforms import (
+    Rotate,
+    euler_angles_to_matrix,
+    random_rotations,
+    so3_exponential_map,
+    so3_log_map,
+    so3_rotation_angle,
+    so3_relative_angle
+)
+from pytorch3d.structures import Meshes
+from random_rasterizer import RandomPhongShader, RandomSimpleShader, SimpleShader, SoftSimpleShader
 from pytorch3d.io import load_objs_as_meshes, save_obj, load_obj
+import torch.autograd.profiler as profiler
+import pinocchio as pin
 
+from smoothagg import SoftAgg, CauchyAgg, GaussianAgg
+from smoothrast import SoftRast, ArctanRast
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
@@ -43,99 +58,149 @@ else:
     
 print("device used",device)
 
-def init_renderers(camera, lights, sigma = 1e-2, gamma = 5e-1):
-  R_init = random_rotations(1)
-  log_rot_init = so3_log_map(R_init)
-  blend_settings=BlendParams(sigma = sigma, gamma = gamma) #smoothing parameters
-
-  raster_settings_soft = RasterizationSettings(
-      image_size=158, 
-      blur_radius= np.log(1. / 1e-4 - 1.)*blend_settings.sigma, 
-      faces_per_pixel=50, 
-  )
-
-  renderer_softras = MeshRenderer(
-      rasterizer=MeshRasterizer(
-          cameras=camera, 
-          raster_settings=raster_settings_soft
-      ),
-      shader=SoftPhongShader(device=device,
-          cameras=camera,
-          lights=lights,
-          blend_params=blend_settings)
-  )
-
-  renderer_random = MeshRenderer(
-      rasterizer=MeshRasterizer(
-          cameras=camera, 
-          raster_settings=raster_settings_soft
-      ),
-      shader=RandomPhongShader(device=device,
-          cameras=camera,
-          lights=lights,
-          blend_params=blend_settings,
-          nb_samples = 1)
-  )
-  return log_rot_init, renderer_softras, renderer_random
+def init_renderers(camera, lights, sigma = 1e-2, gamma = 5e-1, alpha = 1., nb_samples = 16):
+    R_init = random_rotations(1)
+    log_rot_init = so3_log_map(R_init)
+    blend_settings=BlendParams(sigma = sigma, gamma = gamma, background_color = (.0,.0,.0)) #smoothing parameters
+      
+    raster_settings_soft = RasterizationSettings(
+        image_size=128, 
+        blur_radius= np.log(1. / 1e-4 - 1.)*blend_settings.sigma, 
+        faces_per_pixel=12, 
+    )
+    alpha = 1.
+    softras_agg = SoftAgg(gamma= gamma, alpha = alpha )
+    softras_rast = SoftRast(sigma = sigma)
+    
+    renderer_softras = MeshRenderer(
+        rasterizer=MeshRasterizer(
+            cameras=camera, 
+            raster_settings=raster_settings_soft
+        ),
+        # shader=SoftSimpleShader(device=device,
+        #                         blend_params=blend_settings)
+        shader=RandomSimpleShader(device=device,
+            cameras=camera,
+            lights=lights,
+            blend_params=blend_settings,
+            smoothrast = softras_rast,
+            smoothagg =softras_agg)
+    )
+      
+    random_rast = ArctanRast(sigma = sigma)
+    random_agg = CauchyAgg(gamma = gamma, alpha = alpha,nb_samples=nb_samples)
+    renderer_random = MeshRenderer(
+        rasterizer=MeshRasterizer(
+            cameras=camera, 
+            raster_settings=raster_settings_soft
+        ),
+        #shader=RandomPhongShader(device=device,
+        shader=RandomSimpleShader(device=device,
+            cameras=camera,
+            lights=lights,
+            blend_params=blend_settings,
+            smoothrast = softras_rast,
+            smoothagg = random_agg
+            )
+    )
+    return log_rot_init, renderer_softras, renderer_random
 
 def init_target():
-  DATA_DIR = "./data"
-  obj_filename = os.path.join(DATA_DIR, "rubiks/cube2.obj")
-  mesh = load_objs_as_meshes([obj_filename], device=device)
-  verts = mesh.verts_packed()
-  N = verts.shape[0]
-  center = verts.mean(0)
-  scale = max((verts - center).abs().max(0)[0])
-  mesh.offset_verts_(-center.expand(N, 3))
-  mesh.scale_verts_((1.0 / float(scale)));
-
-  num_views = 1
-  
-  elev = torch.linspace(20, 240, num_views)
-  azim = torch.linspace(120,150, num_views)
-
-  lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
-
-  R, T = look_at_view_transform(dist=4.7, elev=elev, azim=azim)
-  #cameras = OpenGLPerspectiveCameras(device=device, R=R, T=T)
-  R,T = R.to(device),T.to(device)
-  cameras = [OpenGLPerspectiveCameras(device=device, R=R[None, i, ...], 
-                                           T=T[None, i, ...]) for i in range(num_views)]
-  camera = OpenGLPerspectiveCameras(device=device, R=R[None, 0, ...], 
-                                    T=T[None, 0, ...]) 
-
-  raster_settings = RasterizationSettings(
-      image_size=158, 
-      blur_radius=0.,
-      faces_per_pixel=1, 
-  )
-
-  renderer = MeshRenderer(
-      rasterizer=MeshRasterizer(
-          cameras=camera, 
-          raster_settings=raster_settings
-      ),
-      shader=HardPhongShader(
-          device=device, 
-          cameras=camera,
-          lights=lights,
-          #blend_params = blend_settings
-      )
-  )
-
-
-  meshes = mesh.extend(num_views)
-  R_true = random_rotations(1)
-  rotation_true = Rotate(R_true, device=device)
-  # rotate the mesh
-  meshes = meshes.update_padded(rotation_true.transform_points(meshes.verts_padded()))
-
-
-  #target_images = renderer(meshes, cameras=cameras, lights=lights)
-  target_images = renderer(meshes, R=R, T=T, lights=lights)
-
-  target_rgb = [target_images[i, ..., :3] for i in range(num_views)]
-  return meshes, cameras, lights, target_rgb, R_true
+    # DATA_DIR = "./data/rubiks"
+    # obj_filename = os.path.join(DATA_DIR, "cube2.obj")
+    # mesh = load_objs_as_meshes([obj_filename], device=device)
+    # verts = mesh.verts_packed()
+    # N = verts.shape[0]
+    # center = verts.mean(0)
+    # scale = max((verts - center).abs().max(0)[0])
+    # mesh.offset_verts_(-center.expand(N, 3))
+    # mesh.scale_verts_((1.0 / float(scale)));
+    
+    datadir = "./data/rubiks"
+    obj_filename = os.path.join(datadir, "cube2.obj")
+    fn = 'cube_p.npz'
+    with np.load(f'{datadir}/{fn}') as f:
+        pos_idx, pos, col_idx, col = f.values()
+      
+    print("Mesh has %d triangles and %d vertices." % (pos_idx.shape[0], pos.shape[0]))
+    if pos.shape[1] == 4: pos = pos[:, 0:3]
+    pos_idx = torch.from_numpy(pos_idx.astype(np.int32)).to(device = device).unsqueeze(0)
+    vtx_pos = torch.from_numpy(pos.astype(np.float32)).to(device = device).unsqueeze(0)
+    col_idx = torch.from_numpy(col_idx.astype(np.int32)).to(device = device).unsqueeze(0)
+    vtx_col = torch.from_numpy(col.astype(np.float32)).to(device = device)
+    
+    #reorder color to have same cube as softras
+    green_col  = vtx_col[3,:].clone()
+    vtx_col[3,:] =vtx_col[0,:]
+    vtx_col[0,:] = green_col
+      
+    verts, faces, aux= load_obj(obj_filename)
+    l = aux.texture_images['cube'].size()[1]//6
+    for i in range(6):
+      aux.texture_images['cube'][:,i*l:(i+1)*l,:] = vtx_col[i,:].unsqueeze(0).unsqueeze(0).repeat(aux.texture_images['cube'].size()[0],l,1)
+    verts_uvs = aux.verts_uvs[None, ...]  # (1, V, 2)
+    faces_uvs = faces.textures_idx[None, ...]  # (1, F, 3)
+    tex_maps = aux.texture_images
+    texture_image = list(tex_maps.values())[0]
+    texture_image = texture_image[None, ...]  # (1, H, W, 3)
+    tex = Textures(verts_uvs=verts_uvs, faces_uvs=faces_uvs, maps=texture_image)
+    mesh = Meshes(verts=[verts], faces=[faces.verts_idx], textures=tex).to(device=device)
+    verts = mesh.verts_packed()
+    N = verts.shape[0]
+    center = verts.mean(0)
+    scale = max((verts - center).abs().max(0)[0])
+    mesh.offset_verts_(-center.expand(N, 3))
+    mesh.scale_verts_((1.0 / float(scale)));
+    
+    
+    num_views = 1
+    
+    elev = torch.linspace(20, 240, num_views)
+    azim = torch.linspace(120,150, num_views)
+    
+    lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
+    
+    R, T = look_at_view_transform(dist=4.7, elev=elev, azim=azim)
+    #cameras = OpenGLPerspectiveCameras(device=device, R=R, T=T)
+    R,T = R.to(device),T.to(device)
+    cameras = [OpenGLPerspectiveCameras(device=device, R=R[None, i, ...], 
+                                             T=T[None, i, ...]) for i in range(num_views)]
+    camera = OpenGLPerspectiveCameras(device=device, R=R[None, 0, ...], 
+                                      T=T[None, 0, ...]) 
+    
+    raster_settings = RasterizationSettings(
+        image_size=128, 
+        blur_radius=0.,
+        faces_per_pixel=1, 
+    )
+    
+    blend_settings = BlendParams(background_color = (0.,0.,0.))
+    
+    renderer = MeshRenderer(
+        rasterizer=MeshRasterizer(
+            cameras=camera, 
+            raster_settings=raster_settings
+        ),
+        
+        shader=SimpleShader(
+            device=device,
+            blend_params= blend_settings
+        )
+    )
+    
+    
+    meshes = mesh.extend(num_views)
+    R_true = random_rotations(1)
+    rotation_true = Rotate(R_true, device=device)
+    # rotate the mesh
+    meshes_rotated = meshes.update_padded(rotation_true.transform_points(meshes.verts_padded()))
+    
+    
+    target_images = renderer(meshes_rotated, cameras=cameras[0], lights=lights)
+    #target_images = renderer(meshes, R=R, T=T, lights=lights)
+    
+    target_rgb = [target_images[i, ..., :3] for i in range(num_views)]
+    return meshes, cameras, lights, target_rgb, R_true
 
 
 def optimize_pose(mesh,cameras,lights,init_pose,diff_renderer,target_rgb,exp_id,lr_init=1e-2,Niter=100,optimizer = "sgd"):
@@ -148,7 +213,7 @@ def optimize_pose(mesh,cameras,lights,init_pose,diff_renderer,target_rgb,exp_id,
   num_views_per_iteration = 1
   num_views = len(target_rgb)
   # Plot period for the losses
-  plot_period = 100
+  plot_period = 1
   gradient_values = []
   loop = tqdm(range(Niter))
   adapt_reg = False
@@ -163,6 +228,7 @@ def optimize_pose(mesh,cameras,lights,init_pose,diff_renderer,target_rgb,exp_id,
       optimizer = torch.optim.Adam([log_rot], lr=lr_init)
   for i in loop:
     R = so3_exponential_map(log_rot)
+    #print(R)
     rotation = Rotate(R, device=device)
     # rotate the mesh
     predicted_mesh = mesh.update_padded(rotation.transform_points(mesh.verts_padded()))
@@ -198,6 +264,8 @@ def optimize_pose(mesh,cameras,lights,init_pose,diff_renderer,target_rgb,exp_id,
     sum_loss.backward()
     #print(grad_pred)
     gradient_values += [torch.norm(log_rot.grad).detach().cpu().item()]
+    #print(torch.norm(log_rot.grad).detach().cpu().item())
+    #print("AD grad",log_rot.grad)
     #print("log rot before", log_rot, "log rot grad", grad_pred)
     optimizer.step()
   fig = plt.figure(figsize=(13, 5))
@@ -215,6 +283,7 @@ def optimize_pose(mesh,cameras,lights,init_pose,diff_renderer,target_rgb,exp_id,
       os.mkdir(path_fig/"optimization_details")
   if not os.path.exists(path_fig/"optimization_details"/datenow):
       os.mkdir(path_fig/"optimization_details"/datenow)
+  np.save(path_fig/"optimization_details"/datenow/'loss_values.npy', losses["rgb"]['values'])
   plt.savefig(path_fig/"optimization_details"/datenow/'loss_values.png', bbox_inches='tight')
   plt.close()
   plt.figure()
@@ -237,6 +306,8 @@ def compare_pose_opt(params_file):
     optimizer = params_dic["optimizer"]
     lr_list = params_dic["lr_list"]
     smoothing_list = params_dic["smoothing_list"]
+    MC_samples = params_dic["MC_samples"]
+    noise_type = params_dic["noise_type"]
     params = {"lr-smoothing":[]}
     for j,lr in enumerate(lr_list):
         for k,smoothing in enumerate(smoothing_list):
@@ -246,7 +317,10 @@ def compare_pose_opt(params_file):
             for i in range(N_benchmark):
               print(i+1,'/', N_benchmark, 'test problem')
               meshes,cameras,lights,target_rgb,R_true = init_target()
-              log_rot_init, renderer_softras, renderer_random = init_renderers(cameras,lights,sigma,gamma)
+              #print(R_true)
+              log_rot_init, renderer_softras, renderer_random = init_renderers(cameras,lights,sigma= sigma,gamma=gamma,nb_samples=MC_samples)
+              #log_rot_init = torch.tensor(pin.log3(R_true.detach().cpu()[0].numpy()), dtype=torch.float32).unsqueeze(0)
+              #log_rot_init = so3_log_map(R_true)
               log_rot = optimize_pose(meshes,cameras,lights,log_rot_init, renderer_softras, target_rgb,exp_id, Niter = Niter, optimizer = optimizer)
               angle_errors["softras"]+=[so3_relative_angle(so3_exponential_map(log_rot), R_true).detach().cpu().numpy()*180./np.pi]
               log_rot = optimize_pose(meshes,cameras,lights,log_rot_init, renderer_random, target_rgb,exp_id, Niter = Niter, optimizer = optimizer)
@@ -277,7 +351,68 @@ def visualize_prediction(predicted_mesh, renderer,R,T,
     plt.axis("off")
     plt.close()
     
+def hat(v):
+    N, dim = v.shape
+    if dim != 3:
+        raise ValueError("Input vectors have to be 3-dimensional.")
+
+    h = v.new_zeros(N, 3, 3)
+
+    x, y, z = v.unbind(1)
+
+    h[:, 0, 1] = -z
+    h[:, 0, 2] = y
+    h[:, 1, 0] = z
+    h[:, 1, 2] = -x
+    h[:, 2, 0] = -y
+    h[:, 2, 1] = x
+
+    return h    
     
+
+def so3_exponential_map_corrected(log_rot):
+    _, dim = log_rot.shape
+    if dim != 3:
+        raise ValueError("Input tensor shape has to be Nx3.")
+    
+    nrms = (log_rot * log_rot).sum(1)
+    # phis ... rotation angles
+    batch_size = nrms.size()[0]
+    R = torch.zeros(batch_size,3,3)
+    for j in range(batch_size):
+        if nrms[j]!=0.:
+            rot_angles = nrms[j].sqrt()
+            rot_angles_inv = 1.0 / rot_angles
+            fac1 = rot_angles_inv * rot_angles.sin().unsqueeze(0)
+            fac2 = (rot_angles_inv * rot_angles_inv * (1.0 - rot_angles.cos())).unsqueeze(0)
+            skews = hat(log_rot[j].unsqueeze(0))
+            R[j] = (
+                fac1[:, None, None] * skews
+                + fac2[:, None, None] * torch.bmm(skews, skews)
+                + torch.eye(3, dtype=log_rot.dtype, device=log_rot.device)[None]
+            ).squeeze(0)
+
+        else:
+            R[j] = exp_map0.apply(log_rot[j])
+    return R
+
+class exp_map0(Function):
+    @staticmethod
+    def forward(ctx,log_rot):
+        R = torch.eye(3, dtype=log_rot.dtype, device=log_rot.device)[None]
+        ctx.save_for_backward(log_rot)
+        return R
+    
+    @staticmethod
+    def backward(ctx,grad_l):
+        log_rot = ctx.saved_tensors[0]
+        grad_log_rot = torch.zeros(log_rot.size())
+        for i in range(log_rot.size()[0]):
+            e_i = torch.zeros(1,3,1)
+            e_i[0,i,0] = 1.
+            grad_log_rot[i] = (hat(e_i.squeeze(-1))*grad_l).sum()
+        return grad_log_rot
+  
 def image_grid(
     images,
     title,
