fix gradients

randomras/smoothagg.py
@@ -92,8 +92,14 @@ class SoftAgg(SmoothAggBase):
         device =zbuf.device
         z_inv = (zfar - zbuf) / (zfar - znear) * mask
         z_inv_max = torch.max(z_inv, dim=-1).values[..., None].clamp(min=self.eps)
-        z_map = ((self.gamma/self.alpha)*torch.log(prob_map)+ z_inv- z_inv_max)
-        z_map =torch.cat((z_map,(torch.ones((z_map.size()[0],z_map.size()[1],z_map.size()[2],1),device=device)*self.eps -z_inv_max)),dim=-1)
+        #prob_map.register_hook(lambda x: print("prob_map grad",torch.max(x),x[0,0:3,0:3,0:3]))
+        #z_map = ((self.gamma/self.alpha)*torch.log(prob_map)+ z_inv- z_inv_max)
+        z_map = ((self.gamma/self.alpha)*log_corrected.apply(prob_map)+ z_inv- z_inv_max)
+        #print(z_map.size())
+        #print(z_map[0,0:3,0:3,0:3])
+        #z_map.register_hook(lambda x: print("zmap2 grad",torch.max(x)))
+        z_map =torch.cat((z_map,(torch.ones((z_map.size()[0],z_map.size()[1],z_map.size()[2],1),device=device)*self.eps -z_inv_max)),dim=-1)    
+        #z_map.register_hook(lambda x: print("zmap3 grad",torch.max(x)))
         weights = torch.softmax(z_map/self.gamma,dim=-1)
         return weights
     
@@ -114,7 +120,8 @@ class GaussianAgg(SmoothAggBase):
         device =zbuf.device
         z_inv = (zfar - zbuf) / (zfar - znear) * mask
         z_inv_max = torch.max(z_inv, dim=-1).values[..., None].clamp(min=self.eps)
-        z_map = ((self.gamma/self.alpha)*torch.log(1e-12+prob_map)+ z_inv-z_inv_max) # substract z_inv_max ?
+        #z_map = ((self.gamma/self.alpha)*torch.log(1e-12+prob_map)+ z_inv-z_inv_max) # substract z_inv_max ?
+        z_map = ((self.gamma/self.alpha)*log_corrected.apply(prob_map)+ z_inv-z_inv_max)
         z_map =torch.cat((z_map,torch.ones((z_map.size()[0],z_map.size()[1],z_map.size()[2],1),device=device)*self.eps-z_inv_max),dim=-1)
         randomarg = randomArgmax.apply
         randomax = randomarg(z_map, self.nb_samples, self.gamma, "gaussian",self.fixed_noise)
@@ -139,10 +146,15 @@ class CauchyAgg(SmoothAggBase):
         z_inv_max = torch.max(z_inv, dim=-1).values[..., None].clamp(min=self.eps)
         #z_map = ((self.gamma/self.alpha)*torch.log(1e-12+prob_map)+ z_inv) # substract z_inv_max ?
         #z_map = ((self.gamma/self.alpha)*torch.log(prob_map.clamp(min=1e-12))+ z_inv-z_inv_max) # substract z_inv_max ? add 1e-12 to prob map ? 
+        #prob_map.register_hook(lambda x: print("probmap grad",torch.max(x),x[0,0:3,0:3,0:3]))
         z_map = ((self.gamma/self.alpha)*log_corrected.apply(prob_map)+ z_inv-z_inv_max)
         #add background component with inverse depth of eps
+        #print(z_map.size())
+        #print(z_map[0,0:3,0:3,0:3])
+        #z_map.register_hook(lambda x: print("zmap2 grad",torch.max(x)))
         z_map =torch.cat((z_map,torch.ones((z_map.size()[0],z_map.size()[1],z_map.size()[2],1),device=device)*self.eps-z_inv_max ),dim=-1)
         #z_map =torch.cat((z_map,torch.ones((z_map.size()[0],z_map.size()[1],z_map.size()[2],1),device=device)*self.eps ),dim=-1)
+        #z_map.register_hook(lambda x: print("zmap3 grad",torch.max(x)))
         randomarg = randomArgmax.apply
         randomax = randomarg(z_map, self.nb_samples, self.gamma, "cauchy", self.fixed_noise)
         return randomax
@@ -205,7 +217,10 @@ class log_corrected(Function):
             (x,) = ctx.saved_tensors
             device = x.device
             grad_log = torch.ones(x.size(),device= device)/x
+            #print("x",x[torch.where(torch.isinf(grad_log))],x[torch.where(torch.isnan(grad_log))])
+            grad_log = torch.where(torch.isinf(grad_log), torch.zeros_like(grad_log), grad_log)
+            #print("grad log", torch.max(grad_log),"grad_l", torch.max(grad_l))
             grad_log = grad_log*grad_l
-            grad_log = torch.where(torch.isnan(grad_log), torch.zeros_like(grad_log), grad_log)
+            #grad_log = torch.where(torch.isnan(grad_log), torch.zeros_like(grad_log), grad_log)
         return grad_log
     

utils/utils.py
@@ -105,7 +105,7 @@ def init_renderers(camera, lights, R_true, pert_init_intensity = 30., sigma = 1e
                 )
         )
         renderers+=[renderer_random]
-    #log_rot_init = torch.tensor([[ 0.45747742,  0.36187533, -0.92777318]], device=device)
+    log_rot_init = torch.tensor([[ 0.49747742,  0.36187533, -0.92777318]], device=device)
     #log_rot_init = torch.tensor([[-0.3333,  1.6948,  2.1758]], device=device)
     #log_rot_init = torch.tensor([[-3.0002, -0.9406,  0.0781]], device=device)
     #log_rot_init = torch.tensor([[0.9700, 1.5611, 0.3937]], device = device)